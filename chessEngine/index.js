3a5c0280e08672ed9eeb274e53ec71dfbbdc8e07
Carlos Wimmer <coiote777@gmail.com>
2021-01-25 23:19:15 -0300

    Get knight attacks and test with black queen coord

--- chessEngine/index.js
+++ chessEngine/index.js
@@ -1,18 +1,35 @@
 const whites = [
-  ["N", "B", 2],
-  ["Q", "B", 1],
+  ['N', 'B', 2],
+  ['Q', 'B', 1],
 ];
 
-const blacks = [["Q", "A", 4]];
+const blacks = [['Q', 'A', 4]];
 
 const moves = 1;
 
-const cols = ["A", "B", "C", "D"];
+const cols = ['A', 'B', 'C', 'D'];
 
-const wnPosition = whites.find((item) => item.includes("N"));
-wnPosition[1] = cols.indexOf(wnPosition[1]);
+function getWhitePosition(t) {
+  const position = whites.find((item) => item.includes(t));
+  position[1] = cols.indexOf(position[1]);
+  const coordinates = {
+    c: position[1],
+    r: position[2],
+  };
 
-function getNAttacks(c, r) {
+  return coordinates;
+}
+
+function getBlackQueenPosition() {
+  const position = blacks.find((item) => item.includes('Q'));
+  position[1] = cols.indexOf(position[1]);
+  position.shift();
+  const stringfied = position.join('');
+
+  return stringfied;
+}
+
+function getNAttacks({ c, r }) {
   const attacks = [
     [c - 2, r + 1],
     [c - 2, r - 1],
@@ -25,10 +42,32 @@ function getNAttacks(c, r) {
   ];
 
   const possibles = attacks.filter((att) => att[0] >= 0 && att[1] >= 1);
+  const stringfied = possibles.map((item) => item.join(''));
 
-  return possibles;
+  return stringfied;
 }
 
-const wnAttacks = getNAttacks(wnPosition[1], wnPosition[2]);
+function getQAttacks({ c, r }) {
+  const attacks = [
+    [c + 1, r],
+    [c + 2, r],
+    [c + 3, r],
+    [c - 1, r],
+    [c - 2, r],
+    [c - 3, r],
+    [c, r + 1],
+    [c, r + 2],
+    [c, r + 3],
+    [c, r - 1],
+    [c, r - 2],
+    [c, r - 3],
+  ];
+}
 
-console.log(wnAttacks);
+const wnAttacks = getNAttacks(getWhitePosition('N'));
+const bqPosition = getBlackQueenPosition();
+const answer = wnAttacks.includes(bqPosition);
+
+console.log('wnAttacks', wnAttacks);
+console.log('bqPosition', bqPosition);
+console.log('answer', answer ? 'YES' : 'NO');
